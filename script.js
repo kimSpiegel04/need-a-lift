d534d2a4275a7537ba97ddd20f33345ab48bd553
kimSpiegel04 <k.spiegel72@gmail.com>
2020-03-10 10:59:57 -0400

    elevator data showing up on map (key left out)

--- script.js
+++ script.js
@@ -12,20 +12,40 @@
 //     locationDiv.innerHTML = "Latitude: " + position.coords.latitude + '<br>Logitude: '+ position.coords.longitude;
 // } 40.709844, -74.008025
 
+var entryArr=[];
 fetch('https://data.ny.gov/resource/i9wp-a4ja.json?$limit=2000')
     .then(response=>{
         return response.json()
     })
     .then((data)=>{
-        var arr=[];
         for(var i=0; i<data.length; i++){
             if(data[i].entrance_type=="Elevator"){
-                console.log(data[i]);
-                arr.push(data[i]);
+                entryArr.push(data[i]);
             }
         }
-        console.log(arr.length);
+        console.log(entryArr[0].entrance_latitude, entryArr[0].entrance_longitude);
     })
     .catch(err=>{
         console.log(err);
     })
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.709844, lng: -74.008025},
+        zoom: 10
+    });
+
+    setMarkers(map);
+}
+
+function setMarkers(map){
+    for(var i=0; i<entryArr.length; i++){
+        var entry = entryArr[i];
+        console.log(parseFloat(entry.entrance_latitude), parseFloat(entry.entrance_longitude));
+        var marker = new google.maps.Marker({
+            position: {lat: parseFloat(entry.entrance_latitude), lng: parseFloat(entry.entrance_longitude)},
+            map: map
+        })
+    }
+}
